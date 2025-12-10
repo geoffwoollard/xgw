aa582bba377e894d958e3e48374076dbba8e3ece
Geoffrey Woollard <geoffwoollard@gmail.com>
2025-12-10 14:51:52 -0800

    Haussdorf

--- tests/test_square_in_circle_in_square.py
+++ tests/test_square_in_circle_in_square.py
@@ -4,21 +4,19 @@ from xgw.Hyperplane_approx import DoubleRepresentation, Hausdorff
 
 
 def test_Hausdorff():
-    inner_square_vertices = [
+    inner_square_vertices = [np.array(v) for v in [
         (0,1), (0,-1), (1,0), (-1,0)
-    ]
+    ]]
 
-    outer_square_vertices = [
+    outer_square_vertices = [np.array(v) for v in [
         (1,1), (1,-1), (-1,1), (-1,-1)
-    ]
+    ]]
 
     previous_solutions_to_reuse = {}
     P_plus = DoubleRepresentation()
-    for v in outer_square_vertices:
-        P_plus.add_V(np.array(v))  
+    P_plus.add_V(outer_square_vertices)  
     P_minus = DoubleRepresentation()
-    for v in inner_square_vertices:
-        P_minus.add_V(np.array(v))
+    P_minus.add_V(inner_square_vertices)
 
     x_0, v_0, objective, previous_solutions_to_reuse = Hausdorff(P_plus, P_minus, previous_solutions_to_reuse)
     assert np.allclose(objective, np.linalg.norm(x_0 - v_0)**2)
