766b4f936ddc214a2ecfdcd2b95f9d54072a7842
Geoffrey Woollard <geoffwoollard@gmail.com>
2026-01-28 13:02:31 -0800

    random halfplane wrt set

--- src/xgw/halfplane_utils.py
+++ src/xgw/halfplane_utils.py
@@ -8,7 +8,7 @@ def random_cut(V, rng=None):
     if rng is None:
         rng = np.random.default_rng()
 
-    n, d = V.shape
+    d = V.shape[1]
 
     # 1. random unit vector
     x = rng.normal(size=d)
@@ -31,24 +31,3 @@ def split_by_cut(V, x, r):
     on    = V[np.isclose(vals, 0)]
     return left, right, on
 
-def test_random_cut_and_split():
-    # Example: cube in 3D
-    V = np.array([
-        [0,0,0],[1,0,0],[0,1,0],[1,1,0],
-        [0,0,1],[1,0,1],[0,1,1],[1,1,1]
-    ])
-
-    x, r = random_cut(V)
-    assert isinstance(r, float)
-    assert x.shape == (3,)
-    print("normal:", x)
-    print("offset:", r)
-    left, right, on = split_by_cut(V, x, r)
-    print("left:", left)
-    print("right:", right)
-    print("on:", on)
-    assert len(left) + len(right) + len(on) == len(V)
-    assert np.allclose(np.unique(np.concatenate([left, right, on]), axis=0), np.unique(V, axis=0))
-
-if __name__ == "__main__":
-    test_random_cut_and_split()
