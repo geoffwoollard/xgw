add040fef402f3b171f6a74b4333fb90f5d8b4a4
Geoffrey Woollard <geoffwoollard@gmail.com>
2026-03-10 18:26:01 -0700

    sh proteins

--- experiments/proteins.sh
+++ experiments/proteins.sh
@@ -1,1 +1,5 @@
+python proteins.py repo_dir=/home/gw/repos/xgw/ dimension=3 max_iter=30 p_plus_implementation=h_to_v_popcount_sparse n_models=3 > output_proteins.txt
 python proteins.py repo_dir=/home/gw/repos/xgw/ dimension=3 max_iter=30 p_plus_implementation=h_to_v_popcount_sparse n_models=5 > output_proteins.txt
+python proteins.py repo_dir=/home/gw/repos/xgw/ dimension=3 max_iter=40 p_plus_implementation=h_to_v_popcount_sparse n_models=20 > output_proteins.txt
+python proteins.py repo_dir=/home/gw/repos/xgw/ dimension=3 max_iter=60 p_plus_implementation=h_to_v_popcount_sparse n_models=5 > output_proteins.txt
+python proteins.py repo_dir=/home/gw/repos/xgw/ dimension=3 max_iter=60 p_plus_implementation=h_to_v_popcount_sparse n_models=20 > output_proteins.txt
