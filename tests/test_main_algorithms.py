2d1b0f0d3edb5c1a419b7bd96fde92196b5645a3
Geoffrey Woollard <geoffwoollard@gmail.com>
2026-04-01 15:13:24 -0700

    100 iterations. clean up sparse popcount

--- tests/test_main_algorithms.py
+++ tests/test_main_algorithms.py
@@ -221,5 +221,5 @@ def test_cgw_convex_rotation_invariant():
 
 
 if __name__ == "__main__":
-    test_3d_convex(['h_to_v_popcount_sparse'], ['v_to_h_dual'], ['h_to_v_popcount_sparse'], iter_max=50)
+    test_3d_convex(['h_to_v_popcount_sparse'], ['v_to_h_dual'], ['h_to_v_popcount_sparse'], iter_max=100)
     
