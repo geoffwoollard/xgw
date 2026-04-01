f062703f5c6124e7884e2f1a2cae411fa6d64e6f
Geoffrey Woollard <geoffwoollard@gmail.com>
2026-04-01 14:53:14 -0700

    _build_adjacency_subset_vectorized_chunked

--- src/xgw/h_to_v_popcount.py
+++ src/xgw/h_to_v_popcount.py
@@ -510,6 +510,8 @@ class ExtremePointPolytopeSparse:
 
         logger.info('# ---------- Step G: assemble new adjacency ----------')
         D_old_working = self._build_adjacency_subset(masks_old, use_sparse=self.use_D_sparse)
+        D_old_also_working = self._build_adjacency_subset_vectorized(masks_old)
+        assert np.array_equal(D_old_working.toarray(), D_old_also_working), "Adjacency subsets do not match!"
         D_old = self._build_adjacency_subset_vectorized_chunked(masks_old, use_sparse=self.use_D_sparse)
         assert np.array_equal(D_old_working.toarray(), D_old.toarray()), "Adjacency subsets do not match!"
 
