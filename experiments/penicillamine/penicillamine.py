243b98d15cb9cff28efd36d4c6a7724b645cb3a4
Geoffrey Woollard <geoffwoollard@gmail.com>
2026-04-28 15:55:31 -0700

    kdd 10 conformers

--- experiments/penicillamine/penicillamine.py
+++ experiments/penicillamine/penicillamine.py
@@ -107,5 +107,5 @@ if __name__ == "__main__":
     
     fname_molecule_input = "/home/gw/repos/xgw/experiments/penicillamine/Conformer3D_COMPOUND_CID_4727.sdf"
     fname = f"/home/gw/repos/xgw/experiments/penicillamine/penicillamine_conformers_nconfcormers{n_conformers}_noiselevel{noise_level}_itermax{iter_max}.npz"
-    # main(fname_molecule_input, fname, iter_max, n_conformers, noise_level )
+    main(fname_molecule_input, fname, iter_max, n_conformers, noise_level )
     plot(fname)
