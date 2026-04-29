62904560ab911f25a68c746739f0d5561986f3f4
Geoffrey Woollard <geoffwoollard@gmail.com>
2026-04-28 17:11:51 -0700

    kdd

--- experiments/penicillamine/penicillamine.py
+++ experiments/penicillamine/penicillamine.py
@@ -111,12 +111,11 @@ def plot(fname):
 
 
 if __name__ == "__main__":
-    n_conformers = 4
+    n_conformers = 5
     noise_level = 0.001
     iter_max = 40
-    flip = False
-    
-    fname_molecule_input = "/home/gw/repos/xgw/experiments/penicillamine/Conformer3D_COMPOUND_CID_4727.sdf"
-    fname = f"/home/gw/repos/xgw/experiments/penicillamine/penicillamine_conformers_nconfcormers{n_conformers}_noiselevel{noise_level}_itermax{iter_max}_flip{flip}.npz"
-    main(fname_molecule_input, fname, iter_max, n_conformers, noise_level, flip)
-    plot(fname)
+    for flip in [False, True]:
+        fname_molecule_input = "/home/gw/repos/xgw/experiments/penicillamine/Conformer3D_COMPOUND_CID_4727.sdf"
+        fname = f"/home/gw/repos/xgw/experiments/penicillamine/penicillamine_conformers_nconfcormers{n_conformers}_noiselevel{noise_level}_itermax{iter_max}_flip{flip}.npz"
+        main(fname_molecule_input, fname, iter_max, n_conformers, noise_level, flip)
+        plot(fname)
